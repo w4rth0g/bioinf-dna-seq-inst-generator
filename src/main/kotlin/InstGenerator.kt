adfed68fbf3a0d16e9599be7057f3b8eef705f3f
inf151759 <mikolaj.gostkowski@student.put.poznan.pl>
2024-05-17 17:53:50 +0200

    Refactor + Dodaj obsługę błędów pozytywnych i negatywnych

--- src/main/kotlin/InstGenerator.kt
+++ src/main/kotlin/InstGenerator.kt
@@ -2,17 +2,33 @@ package pl.bioinf
 
 import pl.bioinf.data.Node
 import pl.bioinf.data.checkMaxPokrycie
+import kotlin.random.Random
 
 class InstGenerator {
 
     companion object {
 
-        fun generateInstance(nodeStrSeq: String, kNum: Int): List<Node> {
+        fun generateInstance(nodeStrSeq: String, kNum: Int, positiveErrors: Int?, negativeErrors: Int?): List<Node> {
             val spectrum = nodeStrSeq.splitBy(kNum)
 
             spectrum.sort()
 
-            val spectrumNodes = spectrum.map {
+            // Dodaj błędy
+            var spectrumWithErrors: Array<String>
+
+            spectrumWithErrors = if (negativeErrors != null) {
+                addNegativeErrors(spectrum, negativeErrors).toTypedArray()
+            } else {
+                spectrum
+            }
+
+            spectrumWithErrors = if (positiveErrors != null) {
+                addPositiveErrors(spectrum, positiveErrors, kNum).toTypedArray()
+            } else {
+                spectrum
+            }
+
+            val spectrumNodes = spectrumWithErrors.map {
                 Node(it)
             }
 
@@ -27,7 +43,12 @@ class InstGenerator {
                     }
             }
 
-            // handle errors
+            handleDuplicateErrors(spectrumNodes)
+
+            return spectrumNodes
+        }
+
+        private fun handleDuplicateErrors(spectrumNodes: List<Node>) {
             spectrumNodes.forEach { node ->
                 node.nexts.asSequence()
                     .map {spectrumNodes.getById(it.key)}
@@ -47,8 +68,34 @@ class InstGenerator {
                         }
                     }
             }
+        }
 
-            return spectrumNodes
+        private fun addNegativeErrors(spectrum: Array<String>, numErrors: Int): List<String> {
+            val mutableSpectrum = spectrum.toMutableList()
+
+            repeat(numErrors) {
+                if (mutableSpectrum.isNotEmpty()) {
+                    mutableSpectrum.removeAt(Random.nextInt(mutableSpectrum.size))
+                }
+            }
+
+            return mutableSpectrum
+        }
+
+        private fun addPositiveErrors(spectrum: Array<String>, numErrors: Int, kNum: Int): List<String> {
+            val mutableSpectrum = spectrum.toMutableList()
+
+            repeat(numErrors) {
+                val randomOligonucleotide = generateRandomOligonucleotide(kNum)
+                mutableSpectrum.add(randomOligonucleotide)
+            }
+
+            return mutableSpectrum
+        }
+
+        private fun generateRandomOligonucleotide(length: Int): String {
+            val nucleotides = listOf('A', 'C', 'G', 'T')
+            return (1..length).map { nucleotides.random() }.joinToString("")
         }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,10 @@ import pl.bioinf.data.DNA_STR
 
 fun main() {
 
-    val spectrumNodes = InstGenerator.generateInstance(DNA_STR, 8)
+    val positiveErrors = 10
+    val negativeErrors = 10
+
+    val spectrumNodes = InstGenerator.generateInstance(DNA_STR, 8, positiveErrors, negativeErrors)
 
     println(spectrumNodes)
 }
